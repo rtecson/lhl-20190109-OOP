130a5c486db25888ab27b9f697e933ce500603dc
Roland Tecson <roland_tecson@hotmail.com>
2019-01-09 09:35:44 -0800

    Initial Commit

---
+++ Ball/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Ball
+//
+//  Created by Roland on 2019-01-09.
+//  Copyright © 2019 Game of Apps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
